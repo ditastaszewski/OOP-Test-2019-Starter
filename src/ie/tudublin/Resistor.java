a4551c4d5c886d457eb442d1e1ed8cc056d218fe
godsblade <alex.staszewski.g@gmail.com>
2019-04-10 17:04:51 +0100

    added resistor class

---
+++ src/ie/tudublin/Resistor.java
@@ -0,0 +1,26 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+public class Resistor extends PApplet
+
+{
+    public int value;
+    public int ones;
+    public int tens;
+    public int hundreds;
+
+    public void setValues(int v, int o, int t, int h) 
+    {
+        value = v;
+        ones = o;
+        tens = t;
+        hundreds = h;
+    }
+    
+
+}
